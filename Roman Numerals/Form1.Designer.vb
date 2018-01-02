605857aae5c87d86c7fce606284300409486e1bf
Greg Pina <pinagm2825@gmail.com>
2018-01-02 04:27:55 -0500

    Initial commit

---
+++ Roman Numerals/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.lblRoman = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(90, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(129, 38)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter a decimal integer in the range of 1-10"
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(90, 125)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(129, 30)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Equivalent Roman Numeral"
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(93, 260)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvert.TabIndex = 2
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(238, 260)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(384, 260)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(298, 39)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumber.TabIndex = 6
+        '
+        'lblRoman
+        '
+        Me.lblRoman.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblRoman.Location = New System.Drawing.Point(298, 125)
+        Me.lblRoman.Name = "lblRoman"
+        Me.lblRoman.Size = New System.Drawing.Size(100, 20)
+        Me.lblRoman.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(542, 347)
+        Me.Controls.Add(Me.lblRoman)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Roman Numeral Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtNumber As TextBox
+    Friend WithEvents lblRoman As Label
+End Class
---
+++ Roman Numerals/Form1.vb
@@ -0,0 +1,88 @@
+﻿'************************************
+'Greg Pina
+'.Net Programming IT 101-401
+'Assignment 9 - Roman Numerals
+'10/13/2017
+
+Option Strict On
+
+Public Class Form1
+
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        'This clears the program of its data
+        txtNumber.Clear()
+        lblRoman.ResetText()
+        txtNumber.Focus()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'This closes the program
+        Close()
+
+    End Sub
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+
+        'Creates local variables
+        Dim intNumber As Integer
+        Dim strRoman As String
+
+        txtNumber.BackColor = Color.White
+
+        Try
+            'validates input is actually a number
+            If Not Integer.TryParse(txtNumber.Text, intNumber) Then
+                MessageBox.Show("Please enter numbers only.")
+                txtNumber.Focus()
+                txtNumber.BackColor = Color.Yellow
+                Exit Sub
+            End If
+
+            'Makes sure number inputted is betwen 1 and 10
+            If intNumber < 1 Or intNumber > 10 Then
+                MessageBox.Show("Please only enter numbers between 1 and 10.")
+                txtNumber.Focus()
+                txtNumber.BackColor = Color.Yellow
+            End If
+
+            'assigns Roman Numerals to number values
+            If intNumber = 1 Then
+                strRoman = "I"
+            ElseIf intNumber = 2 Then
+                strRoman = "II"
+            ElseIf intNumber = 3 Then
+                strRoman = "III"
+            ElseIf intNumber = 4 Then
+                strRoman = "IV"
+            ElseIf intNumber = 5 Then
+                strRoman = "V"
+            ElseIf intNumber = 6 Then
+                strRoman = "VI"
+            ElseIf intNumber = 7 Then
+                strRoman = "VII"
+            ElseIf intNumber = 8 Then
+                strRoman = "VIII"
+            ElseIf intNumber = 9 Then
+                strRoman = "IX"
+            ElseIf intNumber = 10 Then
+                strRoman = "X"
+            Else
+                strRoman = "Incorrect Number."
+            End If
+
+
+            'Displays the Roman Numeral
+            lblRoman.Text = strRoman
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+    End Sub
+
+End Class
